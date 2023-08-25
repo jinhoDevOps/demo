71af5e27e14a94020c491cb1a413ad583d37401c
jinhoDevOps <jhok@n3cloud.co.kr>
2023-08-25 12:12:18 +0900

    폴더로처리

--- src/main/java/com/example/demo/repository/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-import com.example.demo.Book; // Book 클래스 경로에 맞게 임포트
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
