0c97b7dd066e6a49c51080bcad84451b7f79fa19
jinhoDevOps <jhok@n3cloud.co.kr>
2023-08-25 12:09:52 +0900

    fin: 프로젝트 실패
    maven으로 하니까 리눅스 서버에 도커 올려서 하는거 404뜸

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.example.demo")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+//	WAR 하려고 추가한 항목
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
